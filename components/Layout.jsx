bee099d880b2495f18677cb47446dda7026a464b
Jesús Casañas <jesusenrique9773@gmail.com>
2020-10-01 21:41:27 -0300

    Title Page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 import Head from 'next/head';
 import Header from './Header';
 import { useRouter } from 'next/router';
- 
+
 const Layout = ({ children }) => {
 
     const router = useRouter();
 
+    const title = (string) => {
+        let newTitle = clean(string);
+        newTitle = uppercase(newTitle);
+        return newTitle;
+    };
+
+    const uppercase = (string) => {
+        const newString = string.toUpperCase();
+        return newString;
+    };
+
+    const clean = (string) => {
+        let newString = string.replace(/-/g, ' ');
+        newString = newString.replace(/\//g, '');
+        return newString;
+    };
+
     return (
         <>
             <Head>
-                <title>Jesús Casañas Developer | Sitio Web Oficial</title>
+                <title>
+                    {router.pathname === '/' ? (
+                        'Jesús Casañas Developer | Sitio Web Oficial'
+                    ) : (
+                            title(router.pathname) + ' | Jesús Casañas Developer'
+                        )}
+                </title>
+
                 <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></link>
             </Head>
 
