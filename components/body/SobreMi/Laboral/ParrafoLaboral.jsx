230c20287d5bb51b3e98b31a3f9a11c3db33a3d0
Jesús Casañas <jesusenrique9773@gmail.com>
2021-11-08 20:25:31 -0300

    Texto parrafo laboral

--- components/body/SobreMi/Laboral/ParrafoLaboral.jsx
+++ components/body/SobreMi/Laboral/ParrafoLaboral.jsx
@@ -1,4 +1,5 @@
 import styles from "../../../../styles/Sobremi.module.css";
+import Link from "next/link";
 import Fade from "react-reveal/Fade";
 
 const ParrafoLaboral = () => {
@@ -9,16 +10,22 @@ const ParrafoLaboral = () => {
       </Fade>
       <Fade>
         <p className={styles.parrafoLaboral}>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
-          unde eius odio molestias nam aperiam ratione est a sit accusantium
-          excepturi ipsam aspernatur veritatis vel enim, quas illum quisquam
-          dicta?. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Aliquid iste molestias libero aut. Iusto quis modi aliquam inventore
-          ab maiores molestias facere impedit. Odio neque est in? Quidem,
-          voluptates placeat. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Eveniet perspiciatis rem quo fugiat exercitationem
-          odit laboriosam, harum tenetur quam cupiditate deserunt tempore nobis
-          facilis saepe vel distinctio illo sed ab!
+          Mi experiencia como profesional en el área ha transcurrido en dos
+          importantes empresas, desempeñandome en proyectos innovadores y de
+          envergadura. La primera,{" "}
+          <Link href="https://www.itrio.net/">
+            <a className={styles.link} target="_blanck">
+              Itrio S.A.
+            </a>
+          </Link>{" "}
+          en sociedad con{" "}
+          <Link href="https://www.veritran.com/">
+            <a className={styles.link} target="_blanck">
+              Veritran
+            </a>
+          </Link>{" "}
+          desarrollando servicios para el área financiera junto a un equipo de trabajo
+          multidisciplinario ubicado en distintas partes de Latinoamérica
         </p>
       </Fade>
     </article>
