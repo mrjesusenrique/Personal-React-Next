88f06df404b9370ddb71a993bb0d9f22736ba77f
Jesús Casañas <jesusenrique9773@gmail.com>
2022-03-27 19:05:33 -0300

    mapeo contact

--- components/body/Contacto/Contacto.jsx
+++ components/body/Contacto/Contacto.jsx
@@ -2,59 +2,24 @@ import React from "react";
 import styles from "../../../styles/Contacto.module.css";
 import Link from "next/link";
 import Fade from "react-reveal/Fade";
+import contact from "../../../utilities/contact";
 
 const contacto = () => {
   return (
     <Fade>
       <section className={styles.container} id="contacto">
         <div className={styles.redes}>
-          <div className={styles.red}>
-            <Link href="https://www.linkedin.com/in/jes%C3%BAs-casa%C3%B1as-485a86197/">
-              <a target="_black">
-                <img
-                  className={styles.img}
-                  src="/images/icons_contact/icon_linkedin.svg"
-                  alt="LinkedIn"
-                />
-              </a>
-            </Link>
-          </div>
-
-          <div className={styles.red}>
-            <Link href="https://github.com/mrjesusenrique">
-              <a target="_black">
-                <img
-                  className={styles.img}
-                  src="/images/icons_contact/icon_github.svg"
-                  alt="GitHub"
-                />
-              </a>
-            </Link>
-          </div>
-
-          <div className={styles.red}>
-            <Link href="https://www.instagram.com/mrjesusenrique/?hl=es">
-              <a target="_black">
-                <img
-                  className={styles.img}
-                  src="/images/icons_contact/icon_instagram.svg"
-                  alt="Instagram"
-                />
-              </a>
-            </Link>
-          </div>
-
-          <div className={styles.red}>
-            <Link href="mailto:jesusenrique9773@gmail.com">
-              <a target="_black">
-                <img
-                  className={styles.img}
-                  src="/images/icons_contact/icon_gmail.svg"
-                  alt="Gmail"
-                />
-              </a>
-            </Link>
-          </div>
+          {contact.map((item, index) => {
+            return (
+              <div className={styles.red} key={item.key}>
+                <Link href={item.link}>
+                  <a target="_black">
+                    <img className={styles.img} src={item.src} alt={item.alt} />
+                  </a>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </section>
     </Fade>
---
+++ utilities/contact.js
@@ -0,0 +1,30 @@
+import { uid } from "./functions/uid";
+
+const contact = [
+  {
+    key: uid(234, 45),
+    link: "https://www.linkedin.com/in/jes%C3%BAs-casa%C3%B1as-485a86197/",
+    src: "/images/icons_contact/icon_linkedin.svg",
+    alt: "LinkedIn",
+  },
+  {
+    key: uid(123, 978),
+    link: "https://github.com/mrjesusenrique",
+    src: "/images/icons_contact/icon_github.svg",
+    alt: "GitHub",
+  },
+  {
+    key: uid(332324, 765),
+    link: "https://www.instagram.com/mrjesusenrique/?hl=es",
+    src: "/images/icons_contact/icon_instagram.svg",
+    alt: "Instagram",
+  },
+  {
+    key: uid(5, 466),
+    link: "mailto:jesusenrique9773@gmail.com",
+    src: "/images/icons_contact/icon_gmail.svg",
+    alt: "Gmail",
+  },
+];
+
+export default contact;
