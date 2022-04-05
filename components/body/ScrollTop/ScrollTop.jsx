df8716fa3b8a80331e25db42fe3f2fb06ad0c21d
Jesús Casañas <jesusenrique9773@gmail.com>
2022-04-05 14:48:30 -0300

    corregido error a descendiente de a

--- components/body/ScrollTop/ScrollTop.jsx
+++ components/body/ScrollTop/ScrollTop.jsx
@@ -4,15 +4,18 @@ import { Link as LinkScroll } from "react-scroll";
 const ScrollTop = () => {
   return (
     <div className={styles.contentScrollTop}>
-      <a href="" className={styles.anchorScrolltop}>
-        <LinkScroll to="top" smooth={true} duration={2500}>
-          <img
-            className={styles.imgScrollTop}
-            src="/images/flecha-arriba.svg"
-            title="Volver arriba"
-          />
-        </LinkScroll>
-      </a>
+      <LinkScroll
+        to="top"
+        smooth={true}
+        duration={2500}
+        className={styles.anchorScrolltop}
+      >
+        <img
+          className={styles.imgScrollTop}
+          src="/images/flecha-arriba.svg"
+          title="Volver arriba"
+        />
+      </LinkScroll>
     </div>
   );
 };
