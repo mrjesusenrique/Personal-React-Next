5ef0259501722d5aceeb3ed1a19a8b2d759fa9f3
Jesús Casañas <jesusenrique9773@gmail.com>
2022-04-08 19:52:15 -0300

    DeÃlete animación scrollTop

--- components/body/ScrollTop/ScrollTop.jsx
+++ components/body/ScrollTop/ScrollTop.jsx
@@ -1,22 +1,19 @@
 import styles from "../../../styles/ScrollTop.module.css";
 import { Link as LinkScroll } from "react-scroll";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import Fade from "react-reveal/Fade";
 
 const ScrollTop = () => {
   return (
-    <Fade>
-      <div className={styles.contentScrollTop}>
-        <LinkScroll
-          to="top"
-          smooth={true}
-          duration={2500}
-          className={styles.anchorScrolltop}
-        >
-          <ArrowUpwardIcon className={styles.imgScrollTop} />
-        </LinkScroll>
-      </div>
-    </Fade>
+    <div className={styles.contentScrollTop}>
+      <LinkScroll
+        to="top"
+        smooth={true}
+        duration={2500}
+        className={styles.anchorScrolltop}
+      >
+        <ArrowUpwardIcon className={styles.imgScrollTop} />
+      </LinkScroll>
+    </div>
   );
 };
 
