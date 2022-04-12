cc13a0d57e2a299cad5552981557e5767afdca65
Jesús Casañas <jesusenrique9773@gmail.com>
2022-04-11 21:12:59 -0300

    add i18next

---
+++ i18next-config/i18next.js
@@ -0,0 +1,28 @@
+import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-xhr-backend";
+import { initReactI18next } from "react-i18next";
+import languages from "../assets/locales";
+const availableLanguages = ["es", "en"];
+let fallbacking;
+
+if (typeof window !== "undefined") {
+  fallbacking = localStorage.getItem("i18nextLng");
+} else {
+  fallbacking = "es";
+}
+
+i18next
+  .use(LanguageDetector)
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: fallbacking,
+    fallbacking,
+    debug: true,
+    resources: { ...languages },
+    whiteList: availableLanguages,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/globals.css";
 import NoSsr from "@mui/base/NoSsr";
+import "../i18next-config/i18next";
 
 function App({ Component, pageProps }) {
   return (
