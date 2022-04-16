fbb831e4ecca07f3eb8be1d0770bd98911f3cca4
Jesús Casañas <jesusenrique9773@gmail.com>
2022-04-15 23:29:41 -0300

    creación botón changeLanguage respónsive

--- components/body/SobreMi/Academico/ParrafoAcadermico.jsx
+++ components/body/SobreMi/Academico/ParrafoAcadermico.jsx
@@ -1,12 +1,17 @@
 import styles from "../../../../styles/Sobremi.module.css";
 import Fade from "react-reveal/Fade";
 import Academico from "../../../../utilities/academico";
+import { useTranslation } from "react-i18next";
 
 const ParrafoAcademico = () => {
+  const { t } = useTranslation();
+
   return (
     <article className={styles.contenedorParrafo}>
       <Fade>
-        <h1 className={styles.titleSectionAcademico}>Académico</h1>
+        <h1 className={styles.titleSectionAcademico}>
+          {t("header.navBar.academic")}
+        </h1>
       </Fade>
       <Fade>
         <Academico />
--- components/body/SobreMi/Laboral/ParrafoLaboral.jsx
+++ components/body/SobreMi/Laboral/ParrafoLaboral.jsx
@@ -1,12 +1,17 @@
 import styles from "../../../../styles/Sobremi.module.css";
 import Fade from "react-reveal/Fade";
 import Profesional from "../../../../utilities/profesional";
+import { useTranslation } from "react-i18next";
 
 const ParrafoLaboral = () => {
+  const { t } = useTranslation();
+
   return (
     <article className={styles.contenedorParrafoLaboral}>
       <Fade>
-        <h1 className={styles.titleSectionLaboral}>Profesional</h1>
+        <h1 className={styles.titleSectionLaboral}>
+          {t("header.navBar.labor")}
+        </h1>
       </Fade>
       <Fade>
         <Profesional />
