0d4451ef223016acc5ef71f4461cc7de00eeef06
Jesús Casañas <jesusenrique9773@gmail.com>
2022-04-16 00:51:05 -0300

    footer translate

--- components/body/Footer/Footer.jsx
+++ components/body/Footer/Footer.jsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import styles from "../../../styles/Footer.module.css";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
   const [actualYear, setActualYear] = useState(null);
+  const { t } = useTranslation();
 
   useEffect(() => {
     setActualYear(new Date().getFullYear());
   }, []);
 
   return (
-    <footer className={styles.footer}>
-      &copy; Copyright {actualYear}. Desarrollado por
-      {<strong> Jesús Casañas</strong>}.
-    </footer>
+    <footer
+      className={styles.footer}
+      dangerouslySetInnerHTML={{
+        __html:
+          t("footer.copy") + " " + `${actualYear}.` + " " + t("footer.name"),
+      }}
+    />
   );
 };
 
